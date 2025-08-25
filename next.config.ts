08463ed7ae7b9ed4842f46df3cb4330512ff17fd
Fauzan <fauzan.c4@gmail.com>
2025-08-25 08:09:44 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+        pathname: "/dms/image/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-**",
+      },
+    ],
+  },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
