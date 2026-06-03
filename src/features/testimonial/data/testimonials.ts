f3666caa75b9f34e57460f1630b1a7a5c439997d
Fauzan <fauzan.c4@gmail.com>
2026-06-03 14:57:08 +0700

    fix: update image testimonial

--- src/features/testimonial/data/testimonials.ts
+++ src/features/testimonial/data/testimonials.ts
@@ -4,24 +4,21 @@ export const testimonials = [
     role: "Project Manager - Lova Commerce",
     quotes: `One of the best colleage I've ever meet. From him I learn A lot about scripting. Either it's using python,php or any other programming language. Any task assigned to him always have great result.
 Jack of all trade, know and/or understand many programming language.`,
-    avatar:
-      "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    avatar: "/images/friends/erwin-dwi-raharja.jpeg",
     url: "https://www.linkedin.com/in/erwin-dwi-raharja-52b57b126",
   },
   {
     name: "Fauzi Widya Hananta",
     role: "Technical Support Engineer - MG Group",
     quotes: `fauzan is the best work partner i ever met. he did an execellent job, exactly as instructed by the leader. he always help me when i need more hand to handle a job, even though he is busy. flying high captain Fauzan! may the force be with you`,
-    avatar:
-      "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    avatar: "/images/friends/fauzi-widya-hananta.jpeg",
     url: "https://www.linkedin.com/in/fauziwidyahananta",
   },
   {
     name: "Jovan Ferryal E. F.",
     role: "Web Developer - Klikserver",
     quotes: `One of my best friends from college that always chill. He was the kind of guy who was always down to help or just hang out. We went through a lot of late-night projects and last-minute submissions together, and somehow he always made it feel manageable`,
-    avatar:
-      "https://media.licdn.com/dms/image/v2/D5603AQGrFNej8oDnzQ/profile-displayphoto-crop_800_800/B56ZfsYhEvHcAI-/0/1752017540867?e=1757548800&v=beta&t=X6cjt23VrIhWwe89h3bWzvQQaTg4_gmKBJUoH0WDvvk",
+    avatar: "/images/friends/jovan.jpeg",
     url: "https://id.linkedin.com/in/jovanzers",
   },
 ];
